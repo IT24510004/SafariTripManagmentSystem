da00604b47178398fcb76b2978bffbe2deb2c203
IT24100640 <it24100640@my.sliit.lk>
2025-10-20 16:08:30 +0530

    it24100640

--- src/main/java/com/safaritrip/managment/service/UserService.java
+++ src/main/java/com/safaritrip/managment/service/UserService.java
@@ -3,14 +3,13 @@ package com.safaritrip.management.service;
 import com.safaritrip.management.dto.ProfileDto;
 import com.safaritrip.management.dto.UserDto;
 import com.safaritrip.management.model.User;
-
-import java.util.List; // Import List
+import java.util.List;
 
 public interface UserService {
     void saveUser(UserDto userDto);
     User findByUsername(String username);
     void updateUserProfile(String username, ProfileDto profileDto);
-
-    // Add this new method
     List<User> findAllTourists();
+    List<User> findAllDrivers();
+    List<User> findUnassignedDrivers();
 }
